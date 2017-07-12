4f07809ce4a6cabb12c6fb17e7c2f08d2514f8b4
gavinjp <373080091@qq.com>
2017-07-12 23:22:52 +0800

    Update UserTopicDemo.scala

--- UserTopicDemo.scala
+++ UserTopicDemo.scala
@@ -3,7 +3,7 @@ package chapter4
 import java.util.Random
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.{HashPartitioner, SparkContext, SparkConf}
 
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
@@ -28,8 +28,8 @@ object UserTopicDemo {
       val pair = ("gavin" + i, UserInfo(topics))
       userList += pair
     }
-    println(s"=================用户订阅信息=================")
-    userList.foreach(println)
+//    println(s"=================用户订阅信息=================")
+//    userList.foreach(println)
     userList
   }
 
@@ -41,8 +41,8 @@ object UserTopicDemo {
       val link = (userId, LinkInfo(topic))
       linkList += link
     }
-    println(s"=================用户【$userId】浏览数据=================")
-    linkList.foreach(println)
+//    println(s"=================用户【$userId】浏览数据=================")
+//    linkList.foreach(println)
     linkList
   }
 
@@ -50,19 +50,19 @@ object UserTopicDemo {
 
     //内连接
     val joined = userData.join(events)
-    println(s"=================用户订阅主题和浏览信息内连接=================")
-    joined.foreach(println)
+//    println(s"=================用户订阅主题和浏览信息内连接=================")
+//    joined.foreach(println)
     val offTopic = joined.filter {
       case (userId, (userInfo, linkInfo)) => !userInfo.topics.contains(linkInfo.topic)
     }
-    println("=================用户浏览的未订阅主题=================")
-    offTopic.foreach(println)
-    println("用户浏览的未订阅主题（未去重）: " + offTopic.count())
+//    println("=================用户浏览的未订阅主题=================")
+//    offTopic.foreach(println)
+//    println("用户浏览的未订阅主题（未去重）: " + offTopic.count())
 
     //去重
     val offTopicDistinct = offTopic.distinct()
-    println("=================用户浏览的未订阅主题去重后=================")
-    offTopicDistinct.foreach(println)
+//    println("=================用户浏览的未订阅主题去重后=================")
+//    offTopicDistinct.foreach(println)
     val offTopicVisits = offTopicDistinct.count()
     println("Number of Visits to non-subscribed topics: " + offTopicVisits)
   }
@@ -71,19 +71,26 @@ object UserTopicDemo {
     val conf = new SparkConf().setMaster("local").setAppName("userTopic")
     val sc = new SparkContext(conf)
 
+    val startTime = System.currentTimeMillis()
+
     //用户总数量
-    val userNum = 100
+    val userNum = 5000
     val linksList = new ListBuffer[(String, LinkInfo)]
 
     //过去五分钟内访问的用户数量（随机生成）
-    val currentUserNum = random.nextInt(100) + 1
+//    val currentUserNum = random.nextInt(100) + 1
+    val currentUserNum = 5
 
     //用户订阅的主题
     /*val pair1 = ("gavin1", UserInfo(mutable.Set("A", "B", "C")))
     val pair2 = ("gavin2", UserInfo(mutable.Set("B", "C", "D")))
     val userData = sc.parallelize(List(pair1, pair2))*/
     val userList = produceUserData(userNum)
+//    val userData = sc.parallelize(userList)
+
     val userData = sc.parallelize(userList)
+                      .partitionBy(new HashPartitioner(32)) //构建100个分区
+                      .persist()
 
     //用户浏览数据
     /*val linkList1 = produceLinks("gavin1", 8)
@@ -91,15 +98,21 @@ object UserTopicDemo {
     val linkList = linkList1 ++ linkList2
     val events = sc.parallelize(linkList)*/
 
-    println("=================此次五分钟内共有" + currentUserNum + "名用户浏览主题=================")
-    for (i <- 1 to currentUserNum) {
-      val num = random.nextInt(100) + 1
-      linksList ++= produceLinks("gavin" + num, random.nextInt(10) + 1)
-    }
-    val events = sc.parallelize(linksList)
+//    println("=================此次五分钟内共有" + currentUserNum + "名用户浏览主题=================")
 
     //周期性调用函数来处理过去五分钟产生的事件日志
-    processNewLogs(events, userData)
+    for (i <- 1 to 200) {
+      for (i <- 1 to currentUserNum) {
+        val num = random.nextInt(100) + 1
+        linksList ++= produceLinks("gavin" + num, random.nextInt(10) + 1)
+      }
+      val events = sc.parallelize(linksList)
+      processNewLogs(events, userData)
+      linksList.clear()
+    }
+    val endTime = System.currentTimeMillis()
+    val takesTime = endTime - startTime
+    println("消耗的时间/毫秒：" + takesTime)
   }
 }
 
